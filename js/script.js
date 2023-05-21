7a2ad5e030238c673d996a66fa41bd81e00f5f06
Gopal Lohar <gopalloharnew@gmail.com>
2023-05-21 21:06:23 +0530

    copy and close  functionality

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const themeButtons = document.querySelectorAll("[data-theme-button]");
 const navToggleButton = document.querySelector(".nav-toggle-button");
 const snippetsWraper = document.querySelector(".snippets-wraper");
 const codeDialog = document.querySelector("[data-code-dialog]");
+const codeElement = document.querySelector(".code-element");
+const copyCodeButton = document.querySelector(".copy-code-button");
 
 // theme
 themeButtons.forEach((themeButton) => {
@@ -24,20 +26,65 @@ navToggleButton.addEventListener("click", () => {
 });
 
 //snippets
-
 snippets.forEach((snippet) => {
-  console.log(snippet.title);
   snippetsWraper.append(snippetItem(snippet));
 });
+
+function openDialog(snippet) {
+  codeElement.innerHTML = snippet.code;
+  copyCodeButton.classList.remove("copy-code-success");
+
+  [...codeElement.classList].forEach((className) => {
+    let isClassNameOfPrism = /^language-.*/.test(className);
+    if (isClassNameOfPrism) {
+      codeElement.classList.remove(className);
+    }
+    codeElement.classList.add(`language-${snippet.language}`);
+  });
+
+  codeDialog.querySelector(".code-snippet-title").innerHTML = snippet.title;
+  usePrism();
+  codeDialog.showModal();
+}
 
 function snippetItem(snippet) {
   const li = document.createElement("li");
   li.classList.add("snippet");
   li.textContent = snippet.title;
   li.addEventListener("click", () => {
-    codeDialog.querySelector("code").innerHTML = snippet.code;
-    usePrism();
-    codeDialog.showModal();
+    openDialog(snippet);
   });
   return li;
 }
+
+codeDialog.addEventListener("click", (e) => {
+  const dialogDimensions = codeDialog.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom ||
+    e.target.closest(".close-dialog-button")
+  ) {
+    codeDialog.close();
+  }
+});
+
+copyCodeButton.addEventListener("click", async () => {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    try {
+      navigator.clipboard.writeText(codeElement.innerText);
+      copyCodeButton.classList.add("copy-code-success");
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else {
+    const textarea = document.createElement("textarea");
+    textarea.classList.add("hidden");
+    textarea.value = codeElement.innerText;
+    codeDialog.append(textarea);
+    textarea.select();
+    textarea.setSelectionRange(0, textarea.value.length);
+    document.execCommand("copy");
+  }
+});
